7df86e36e615ec92ea58ebe97138dfee67105c0b
Gerald Glasgow <geraldjglasgow@gmail.com>
2026-09-21 20:02:50 -0400

    openkeep packaging

--- OpenKeep/OpenKeep/src/Plugin.cs
+++ OpenKeep/OpenKeep/src/Plugin.cs
@@ -27,7 +27,7 @@ namespace OpenKeep
     {
         public const string PluginGuid = "milkyteam.openkeep";
         public const string PluginName = "OpenKeep";
-        public const string PluginVersion = "1.1.1";
+        public const string PluginVersion = "1.2.0";
 
         public static ManualLogSource Log { get; private set; }
         public static SyncedConfiguration Synced { get; private set; }
